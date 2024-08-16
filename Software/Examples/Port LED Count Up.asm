a48b6b904a009d4d9995f74cbe1df8212d84b654
turbo-gecko <55830240+turbo-gecko@users.noreply.github.com>
2024-08-16 21:54:07 +1200

    Updated examples

--- Software/Examples/Port LED Count Up.asm
+++ Software/Examples/Port LED Count Up.asm
@@ -2,6 +2,8 @@
 ; Port LED Count Up - Example program that demonstrates outputting to
 ; a port all bit patterns from 0 to 255 then restarts.
 ;
+; v1.1 - 16th August 2024
+;	 Refactored the style of the code.
 ; v1.0 - 14th August 2024
 ;---------------------------------------------------------------------
 
@@ -20,31 +22,31 @@ PORT_1		.equ	40h
 		.org 1800h		; Where to load the program in memory
 
 MAIN:
-	xor a				; Start from 00h
+	xor	a			; Start from 00h
 LOOP:
-	out (PORT_1),a			; Light the LEDs
-;	out (PORT_2),a			; Light the LEDs
+	out	(PORT_1),a		; Light the LEDs
+;	out	(PORT_2),a		; Light the LEDs
 
-	call DELAY			; Wait for a bit...
+	call	DELAY			; Wait for a bit...
 
-	inc a				; Set up for the next count
-	jr LOOP				; Start over again.
+	inc	a			; Set up for the next count
+	jr	LOOP			; Start over again.
 
 ;---------------------------------------------------------------------
 ; Functions
 ;---------------------------------------------------------------------
 
 DELAY:
-	push af				; Save A as we are using A in main
-	ld bc,DELAY_COUNT		; Setup the number of times to loop
+	push	af			; Save A as we are using A in main
+	ld	bc,DELAY_COUNT		; Setup the number of times to loop
 
 DELAY_LOOP
-	dec bc				; Count down by 1
-	ld a,b				; Check to see if B is 0
-	and a
-	jr nz,DELAY_LOOP		; If we haven't reached 0 then go again
+	dec	bc			; Count down by 1
+	ld	a,b			; Check to see if B is 0
+	and	a
+	jr	nz,DELAY_LOOP		; If we haven't reached 0 then go again
 
-	pop af				; Restore A
+	pop	af			; Restore A
 
 	ret
 
