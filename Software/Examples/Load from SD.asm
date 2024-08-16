d2f2dc3ccef78870a18957ba13271da8d23396a6
turbo-gecko <55830240+turbo-gecko@users.noreply.github.com>
2024-08-16 21:53:46 +1200

    Create Load from SD.asm

---
+++ Software/Examples/Load from SD.asm
@@ -0,0 +1,33 @@
+;---------------------------------------------------------------------
+; Load from SD - Example program that demonstrates loading a program
+; from the SD card using the RST 20H call.
+;
+; A -- Disk number
+; BC -- Slot number
+;
+; v1.0 - 16th August 2024
+;---------------------------------------------------------------------
+
+;---------------------------------------------------------------------
+; Constants
+;---------------------------------------------------------------------
+DELAY_COUNT	.equ	2000h
+
+PORT_1		.equ	40h
+;PORT_2		.equ	0fdh
+
+;---------------------------------------------------------------------
+; Main program
+;---------------------------------------------------------------------
+
+		.org 	1800h		; Where to load the program in memory
+
+MAIN:
+	ld	a,03h			; Disk number
+	ld	bc,0000h		; Slot number
+
+	rst	20h			; Call the SD load function
+
+	ret
+
+		.end
